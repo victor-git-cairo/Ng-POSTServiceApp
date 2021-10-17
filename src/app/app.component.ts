66c885b7421e7772705c07b69378dcea077f030b
victor-git-cairo <victor.w.cairo@gmail.com>
2021-10-17 18:03:57 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { Person } from './person';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'service-post';
+export class AppComponent implements OnInit {
+ 
+  title = 'httpGet Example';
+  people:Person[] | undefined;
+  person = new Person();
+  
+  constructor(private apiService:ApiService) {}
+ 
+  ngOnInit() {
+    this.refreshPeople()
+  }
+ 
+  refreshPeople() {
+    this.apiService.getPeople()
+      .subscribe(data => {
+        console.log(data)
+        this.people=data;
+      })      
+ 
+  }
+ 
+  addPerson() {
+    this.apiService.addPerson(this.person)
+      .subscribe(data => {
+        console.log(data)
+        this.refreshPeople();
+      })      
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// json-server --watch db.json
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
